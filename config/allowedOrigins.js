f0a5e3f076d2825457ea06680b7f0b02052c6527
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-04-28 04:28:55 +0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,5 @@
-const allowedOrigins = [
-  'http://localhost:5000',
-  'https://www.writinghat.com',
-  'https://writinghat.com'
-]
-
-module.exports = allowedOrigins
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionsSuccessStatus: 200
+}
